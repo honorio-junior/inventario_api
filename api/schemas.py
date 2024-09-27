acb06ed7c83db1b663fdc8fead87989e555ae118
honorio-junior <honoriojunior321@gmail.com>
2024-09-27 15:33:02 -0400

    feat: adicionado validação do tipo data

--- api/schemas.py
+++ api/schemas.py
@@ -1,16 +1,17 @@
 from typing import Optional
 from pydantic import BaseModel
+from datetime import date
 
 class EsquemaCategoria(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     nome: str
 
 class EsquemaEstoque(BaseModel):
-    id: Optional[int]
-    data: str
+    id: Optional[int] = None
+    data: date
 
 class EsquemaProduto(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     id_estoque: int
     id_categoria: int
     nome: str
